7fd41a436fae76c5b50c135a3cf987982ae82ace
nikita <allin5885@mail.ru>
2019-06-17 11:53:39 +0300

    Task4 is fixed

--- task4/src/by/taining/task4/controller/Main.java
+++ task4/src/by/taining/task4/controller/Main.java
@@ -4,6 +4,7 @@ import by.taining.task4.services.CandyHandler;
 import by.taining.task4.entity.Candy;
 import by.taining.task4.services.ParseService;
 import by.taining.task4.services.SAXParserService;
+import by.taining.task4.view.FileWriter;
 
 import java.util.ArrayList;
 
@@ -15,12 +16,12 @@ public class Main {
         String filename = "data/data.xml";
         String schemaname = "data/candyschema.xsd";
 
+
+
         ParseService ps = new SAXParserService();
         ArrayList list = ps.parse(filename,schemaname);
-        for (Object o : list) {
-            System.out.println(((Candy)o).getName());
-
-        }
+        FileWriter fileWriter = new FileWriter();
+        fileWriter.write("data/output.txt",list);
 
     }
 }
---
+++ task4/src/by/taining/task4/view/Writer.java
@@ -0,0 +1,9 @@
+package by.taining.task4.view;
+
+import java.util.List;
+
+public interface Writer {
+
+    public void write(String text, List list);
+
+}
